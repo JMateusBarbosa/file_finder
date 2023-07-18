f50c4e626418bf670f3ec0a8177efa48f1d39b59
Joao Mateus <jmbp.ecp19@uea.edu.br>
2023-07-18 18:02:44 -0400

    Implementado a pesquisa por data de modificação

---
+++ finder1/exceptions.py
@@ -0,0 +1,5 @@
+
+class FileFinderError(Exception):
+
+    pass
+
---
+++ finder1/finder.py
@@ -0,0 +1,42 @@
+import click
+from pathlib import Path
+from utils import find_by_name
+from utils import find_by_ext
+from utils import find_by_mod
+from utils import timestamp_to_string
+
+
+def process_search(path, key, value):
+    search_mapping ={
+        "name": find_by_name,
+        "ext": find_by_ext,
+        "mod": find_by_mod,
+    }
+
+    files = search_mapping[key](path, value)
+
+    if not files:
+        click.echo(f"Nenhum arquivo encontrado com {key} {value} foi encontrda")
+    else:
+        for f in files:
+            click.echo(
+                f"Nome: {f.name}\n"
+                f"Data Criação: {timestamp_to_string(f.stat().st_birthtime)}\n"
+                f"Data Modificação: {timestamp_to_string(f.stat().st_mtime)}\n"
+                f"Localização: {f.parent.absolute()}"
+            )
+@click.command()
+@click.argument("path", default="")
+@click.option("-k", "--key", required=True, type=click.Choice(["name","ext", "mod"]))
+@click.option("-v", "--value", required=True)
+def finder(path, key, value):
+    root = Path(path)
+
+    if not root.is_dir():
+        raise Exception("O caminho informado não representa um diretorio.")
+
+    click.echo(f"O diretório selecionado foi: {root.absolute()}")
+
+    process_search(path=root, key=key, value=value)
+
+finder()
